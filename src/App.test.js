c2c1cdff5743aa581b750229457469a9bef0514c
xinyan999 <xin.yan.vic@gmail.com>
2023-04-19 23:14:56 +1000

    fixed test unit to deploy

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './components/App/App.js'
 
 test('renders learn react link', () => {
   render(<App />);
-  // const linkElement = screen.getByText(/learn react/i);
   const linkElement = screen.getByText(/Light up your creativity with Lego/i);
   expect(linkElement).toBeInTheDocument();
 });
