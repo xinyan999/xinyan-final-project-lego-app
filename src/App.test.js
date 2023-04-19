86151e2349c45996edcd7ea4b6d2449d2a7c0dcc
xinyan999 <xin.yan.vic@gmail.com>
2023-04-19 23:04:28 +1000

    fourth change to ddeploy

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
-// import App from './App';
 import App from './components/App/App.js'
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Light up your creativity with Lego/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,7 +21,7 @@ function App() {
           <NavLink className='lego-icon' to='/'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/1000px-LEGO_logo.svg.png?20221012140704' alt='Lego Logo'/></NavLink>
           <NavLink className='discovery' to='/lego'>DISCOVERY</NavLink>
           <form role='search'>
-          <label for='searchInput' className='search-label'>Search</label>
+          <label htmlFor='searchInput' className='search-label'>Search</label>
             <input type='text' value={searchQuery} onChange={event => setSearchQuery(event.target.value)} className='search-input' id='searchInput' name='search' aria-labelledby='searchInput' autoComplete='off' autoFocus/>
             <Link to={`/search/${searchQuery}`} className='search' onClick={searchIsClicked}>
               <span aria-hidden='true'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Vector_search_icon.svg/800px-Vector_search_icon.svg.png' alt='search icon' className='search-icon'/></span>
